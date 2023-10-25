316882b5227a9fb585d4cb74a6cffa24b1194607
ljwen17 <ljwen17@grgbanking.com>
2023-10-25 16:47:19 +0800

    增加sd

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -3,12 +3,9 @@ import { createI18n } from 'vue-i18n'
 import enUS from './en-US'
 import zhCN from './zh-CN'
 import zhTW from './zh-TW'
-import { useAppStoreWithOut } from '@/store/modules/app'
 import type { Language } from '@/store/modules/app/helper'
 
-const appStore = useAppStoreWithOut()
-
-const defaultLocale = appStore.language || 'zh-CN'
+const defaultLocale = 'zh-CN'
 
 const i18n = createI18n({
   locale: defaultLocale,
